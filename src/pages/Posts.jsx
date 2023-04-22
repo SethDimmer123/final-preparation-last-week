74117f8f0a7fe7d761ec6d4b0c2e6b7c734b4a0c
Seth Dimmer <sethdimmer1@gmail.com>
2023-04-22 15:40:11 -0400

    initial commit

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,30 +1,67 @@
-import React, { useEffect } from 'react'
-// how to do query strings 
-import { useParams } from 'react-router-dom'
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+// how to do query strings
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 function Posts() {
-    const { id } = useParams();
-    
-    useEffect(() => {// this runs everytime page mounts(loads)
-        async function fetchPosts(){//in react i want a async inside a useEffect i put an async function
-            const { data } = await axios.get("https://jsonplaceholder.typicode.com/posts?userId=1")// 1 is hardcoded 
-            // const response
-        //  destructing my response
-            // console.log("mounted")
-            // console.log(response.data);//now im getting the data
-            console.log(data);
-        }
-        fetchPosts() // call the function
-    },[])
+  const { id } = useParams();
+  const [posts, setPosts] = useState([]); // i need an empty array for the map method to work
+  // setting the posts to a useState i use it to declare a variable so i can use it in the return.
+
+  const [loading,setLoading] = useState(true)// adding a loading state
+  // i set it to true because as soon as the loading mounts the
+//   useEffect will be loading 
+// after the fetching of the posts it will stop loading
+
+  useEffect(() => {
+    //this is how to fetch posts on mount
+    async function fetchPosts() {
+      const { data } = await axios.get(
+        `https://jsonplaceholder.typicode.com/posts?userId=${id}`
+      );
+      setPosts(data);
+      setLoading(false)
+      //the posts will re-render if loading changes
+    }
+    fetchPosts();
+  }, []); // id is now showing up in console i need to render posts
+
   return (
-    <div>
-        {id}
-    </div>
-  )
+    <>
+      <div className="post__search">
+        <button>← Back</button>
+        <div className="post__search--container">
+          <label className="post__search--label">Search by Id</label>
+          <input type="number" />
+          <button>Enter</button>
+        </div>
+      </div>
+      {
+        loading ? (
+            new Array(10).fill(0).map((element,index) =>(// looped over the html below 10 times
+      <div className="post" key={index}>
+        <div className="post__title">
+          <div className="post__title--skeleton"></div>
+        </div>
+        <div className="post__body">
+          <p className="post__body--skeleton"></p>
+        </div>
+      </div>
+            ))
+        ) : (
+            posts.map((post,index) => (
+                //mapping over every post
+                 <div className="post" key={index}>
+                  <div className="post__title">{post.title}</div>
+                  <p className="post__body">{post.body}</p>
+                </div>
+            ))
+        )}
+    </>
+  );
 }
 
-export default Posts
+export default Posts;
 
 // added axios 15:49
 
