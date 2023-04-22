7ba3e4606cbe5818a0f58c26ecb6dfd727d55cc2
Seth Dimmer <sethdimmer1@gmail.com>
2023-04-22 14:27:36 -0400

    initial commit

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,24 +1,28 @@
 import React, { useEffect } from 'react'
 // how to do query strings 
 import { useParams } from 'react-router-dom'
+import axios from 'axios';
 
 function Posts() {
     const { id } = useParams();
     
     useEffect(() => {// this runs everytime page mounts(loads)
         async function fetchPosts(){//in react i want a async inside a useEffect i put an async function
-            const data = await fetch("https://jsonplaceholder.typicode.com/posts/1")
+            const response = await axios.get("https://jsonplaceholder.typicode.com/posts/1")
             // console.log("mounted")
-            console.log(data)
+            console.log(response.data);//now im getting the data
         }
         fetchPosts() // call the function
     },[])
   return (
     <div>
         {id}
-
     </div>
   )
 }
 
 export default Posts
+
+// added axios 15:49
+
+// npm add axios
