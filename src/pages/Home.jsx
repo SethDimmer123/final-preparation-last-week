9da56a140d7f024e6ae5e0051ba0bf8cac4cabb6
Seth Dimmer <sethdimmer1@gmail.com>
2023-04-22 18:12:38 -0400

    initial commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,13 @@ import React from 'react'
 
 function Home() {//calling users route by calling on mount
   // https://jsonplaceholder.typicode.com/users i need to hit this API
+  // create a user useState
+  // create a useEffect and run it on mount
+  // create a async function that calls the users API dynamically
+
+  // once i use axios to get my data
+  // i want to set the users from the useState hook that i made
+  // then i am aloowed to map over the users and show 10 cards
   return (
     <div className="container">
   <div className="row">
