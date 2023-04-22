b4c276777630d1a1f658bbec9c2192061805ee11
Seth Dimmer <sethdimmer1@gmail.com>
2023-04-22 13:47:16 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+// installed 06:04 npm add react-router-dom@6
+import { BrowserRouter as Router,Routes,Route} from 'react-router-dom'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return (// i wrap my entire app IN MY BrowserRouter
+  // ONLY WRAP MY Routes in a Route
+  //element= is my home component
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path='/' element={ <Home />}></Route>{/**home route "/" */}
+          <Route path=':id'></Route>{/**dynamic route is the parent of the home */}
+        </Routes>
+
+      </div>
+    </Router>
   );
 }
 
