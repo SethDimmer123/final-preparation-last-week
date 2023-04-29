7a293e13361e85ecea9f9f02fb7e037cc1436879
Seth Dimmer <sethdimmer1@gmail.com>
2023-04-29 11:15:13 -0400

    initial commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,8 @@
 import React from 'react'
 // install react-router-dom when first creating react app
 
-function Home() {//calling users route by calling on mount
+function Home() {
   // https://jsonplaceholder.typicode.com/users i need to hit this API
-  // create a user useState
-  // create a useEffect and run it on mount
-  // create a async function that calls the users API dynamically
-
-  // once i use axios to get my data
-  // i want to set the users from the useState hook that i made
-  // then i am aloowed to map over the users and show 10 cards
   return (
     <div className="container">
   <div className="row">
