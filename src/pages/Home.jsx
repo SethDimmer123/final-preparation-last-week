a7b87460fb79642ed7e654b12b85eca72a40aa86
Seth Dimmer <sethdimmer1@gmail.com>
2023-04-29 11:50:28 -0400

    initial commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 // install react-router-dom when first creating react app
 import { useParams } from 'react-router-dom'; 
 
 function Home() {
   // https://jsonplaceholder.typicode.com/users i need to hit this API
-  const params = useParams();// gives me an object in console
+  const {id } = useParams();// gives me an object in console
+  // console.log(fetch("https://jsonplaceholder.typicode.com/users")) //1. this will give me an error 
+  // so i call it on mount.
+
+
+  useEffect(() => {   // 2. calling API on mount which means the page will run everytime page loads
+    async function fetchUsers() {// 4. i created an async function inside since i have a useEffect and then put the await 
+      const data = await fetch("https://jsonplaceholder.typicode.com/users")// 3. i used fetch first to fetch data
+      console.log(data)// i have my promise in console
+      // 4. with axios i only need to await 1 time and not 2
+    }
+  }, []);
   return (
     <div className="container">
   <div className="row">
@@ -12,7 +23,7 @@ function Home() {
       <div className="user">
         <div className="user-card">
           <div className="user-card__container">
-            <h3>user.name</h3>
+        3.    <h3>user.name</h3>
             <p>
               <b>Email:</b> user.email
             </p>
